e72c3b363a53aff14f287ecaa15c4f7dfeacfd8d
Stefan Schramm <mail@stefanschramm.net>
2019-03-26 15:00:20 +0100

    Test case with dir + files

--- test/ext2.js
+++ test/ext2.js
@@ -2,12 +2,12 @@ const assert = require('assert');
 const ext2 = require('../');
 const fs = require('fs');
 const crypto = require('crypto');
-
-const tmpFileName = 'tmp/test.img';
+const tmp = require('tmp');
 
 describe('ext2', function() {
 	describe('#initExt2()', function() {
 		it('should initialize an ext2 filesystem (test_8mb_1kb_empty.img)', function() {
+			const tmpFileName = tmp.fileSync().name;
 			const fd = fs.openSync(tmpFileName, 'w+');
 			ext2.initExt2(fd, 1024 * 1024 * 8, 1024, {time: 1553596983});
 			fs.closeSync(fd);
@@ -16,20 +16,24 @@ describe('ext2', function() {
 			assert.equal(md5, '7484882dfc43285b5e9eeffdc097d283');
 			fs.unlinkSync(tmpFileName);
 		});
-		it('should initialize an ext2 filesystem, create a directory and store some small files in it', function() {
+		it('should initialize an ext2 filesystem, create a directory and store some small files in it (test_8mb_1kb_one_dir_two_files.img)', function() {
+			const time = 1553596983;
+			const tmpFileName = tmp.fileSync().name;
 			const fd = fs.openSync(tmpFileName, 'w+');
-			const f = ext2.initExt2(fd, 1024 * 1024 * 8, 1024, {time: 1553596983});
+			const f = ext2.initExt2(fd, 1024 * 1024 * 8, 1024, {time: time});
 			(async () => {
-				ext2.createDirectory(f, "/brownieplayer", {uid: 1000, gid: 1000, accessRights: 0755});
-				await ext2.writeFileFromHostFileSystem(f, "/brownieplayer/test1.txt", "testfiles/brownieplayer/test1.txt");
-				await ext2.writeFileFromHostFileSystem(f, "/brownieplayer/test2.txt", "testfiles/brownieplayer/test2.txt");
+				ext2.createDirectory(f, "/brownieplayer", {uid: 1000, gid: 1000, accessRights: 0755, time: time});
+				await ext2.writeFileFromHostFileSystem(f, "/brownieplayer/2kb.txt", "testfiles/brownieplayer/2kb.txt");
+				await ext2.writeFileFromHostFileSystem(f, "/brownieplayer/1kb.txt", "testfiles/brownieplayer/1kb.txt");
 				fs.closeSync(fd);
-				assert.equal(1, 1); // TODO
+				const fileContent = fs.readFileSync(tmpFileName);
+				const md5 = crypto.createHash('md5').update(fileContent).digest('hex');
+				assert.equal(md5, '8df58e98ae208882d05934ec4a65cc61');
 				fs.unlinkSync(tmpFileName);
-			});
+			})();
 		});
 		it('should throw exception when block size is invalid', function() {
-			const tmpFileName = 'tmp/test.img';
+			const tmpFileName = tmp.fileSync().name;
 			let fd = fs.openSync(tmpFileName, 'w+');
 			try {
 				ext2.initExt2(fd, 1024 * 1024 * 8, 1234);
@@ -41,7 +45,7 @@ describe('ext2', function() {
 			fs.unlinkSync(tmpFileName);
 		});
 		it('should throw exception when partition size is not multiple of block size', function() {
-			const tmpFileName = 'tmp/test.img';
+			const tmpFileName = tmp.fileSync().name;
 			let fd = fs.openSync(tmpFileName, 'w+');
 			try {
 				ext2.initExt2(fd, 1234 * 1234 * 8, 1024);
